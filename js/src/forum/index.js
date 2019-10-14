3d2707ff5985ec5a5c0c0eaac04b3d9b65ff0390
Fajuu <akociszewsky@gmail.com>
2019-10-14 20:43:16 +0200

    Fixed the change of icons

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -9,45 +9,15 @@ import Icon from '../common/models/Icon';
 app.initializers.add('fajuu-icons', app => {
   app.store.models.icons = Icon;
 
-  if (window.FontAwesomeKitConfig) extend(Page.prototype, 'config', function(config) {
+  if (window.FontAwesomeKitConfig) extend(Page.prototype, 'config', function (isInitialized) {
+    if (isInitialized) return;
+
     app.store.all('icons').map(icon => {
       if (icon.isActive()) {
-
         document.querySelectorAll(icon.elementPath()).forEach(e => {
           var regex = new RegExp(icon.originalIcon(), "g");
           if (e) e.setAttribute("class", e.getAttribute("class").replace(regex, icon.modifiedIcon()));
         });
-
-
-        /*
-        if(window.FontAwesomeKitConfig.method == "css") {
-        	document.querySelectorAll(icon.elementPath()).forEach(e => {
-        		var regex = new RegExp(icon.originalIcon(),"g");
-        		if(e) e.setAttribute("class", e.getAttribute("class").replace(regex, icon.modifiedIcon()));
-        	});
-        }
-				
-        if(window.FontAwesomeKitConfig.method == "js") {
-
-        	var ep = icon.elementPath();
-        	if(ep.slice(-1) == 'i' || ep.slice(-1) == 'a') {
-        		var element = ep.slice(0, -1) + "svg";
-        	}
-
-        	document.querySelectorAll(element).forEach(e => {
-
-        		var o = icon.originalIcon().split(" ");
-        		var m = icon.modifiedIcon().split(" ");
-        		
-        		var regexIcon = new RegExp(o[1],"g");
-        		e.setAttribute("class", e.getAttribute("class").replace(regexIcon, m[1]));
-
-        		var regexStyle = new RegExp(o[0],"g");
-        		e.setAttribute("data-prefix", e.getAttribute("data-prefix").replace(regexStyle, m[0]));
-        	});
-        }
-        */
-
       }
     });
   });
